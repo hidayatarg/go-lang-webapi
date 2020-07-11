dd530dfe6f5c51411c43b73c607b8a1a47fa7944
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-07-12 01:39:08 +0300

    Created Custom Handler Pass Parameters via func MUX

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -10,8 +12,16 @@ func main() {
 	var w wolverine
 
 	mux := http.NewServeMux()
+
+	x1 := &messageHandler{"Bu bir mesaj!"}
+	x2 := &messageHandler{"Bu da yeni bir mesaj!"}
+
+	mux.Handle("/bir", x1)
+	mux.Handle("/iki", x2)
+
 	mux.Handle("/ironman", i)
 	mux.Handle("/wolverine", w)
+	log.Println("Listening Port: 8080")
 
 	http.ListenAndServe(":8080", mux)
 }
@@ -26,4 +36,12 @@ type wolverine int
 
 func (x wolverine) ServeHTTP(res http.ResponseWriter, r *http.Request) {
 	io.WriteString(res, "Mr. Wolverine!")
+}
+
+type messageHandler struct {
+	message string
+}
+
+func (x *messageHandler) ServeHTTP(res http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(res, x.message)
 }
